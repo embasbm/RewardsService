2687b71115c28185e62f8d420f04d33c52ea3bd4
Emba Moussa <emoussa@intellicentrics.com>
2015-03-28 15:18:58 +0000

    Subscription model and basic tests

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ActiveRecord::Base
   validates_presence_of :name, :email
   validates :email, uniqueness: true
-
+  has_many :suscriptions
+  has_many :channels, -> { uniq }, through: :suscriptions
 end
